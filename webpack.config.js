fdbc8a1513c9ddf1cd25c00af4cd486aec9858f3
jonzielen <jzielenkievicz@nycgo.com>
2017-04-04 15:37:10 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].css"
+});
+
+const config = [{
+  entry: {
+    main: './sass/sass.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'css'),
+    filename: 'main.css'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      loader: extractSass.extract({
+        use: [{
+          loader: "css-loader",
+          options: {
+            minimize: true
+          }
+        }, {
+          loader: "sass-loader"
+        }],
+        // use style-loader in development
+        fallback: "style-loader"
+      })
+    }]
+  },
+  plugins: [
+    extractSass
+  ]
+}];
+
+module.exports = config;
